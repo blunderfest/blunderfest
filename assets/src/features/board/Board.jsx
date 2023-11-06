ed85b62164f9c44edbd0fb5492768b3adb09ca17
blunderfest <github@blunderfest.org>
2023-11-06 14:41:39 +0100

    ...

--- assets/src/features/board/Board.jsx
+++ assets/src/features/board/Board.jsx
@@ -1,21 +1,19 @@
 import { useAppDispatch, useAppSelector } from "@/store";
 import { move } from "@/store/games";
 import { reset } from "@/store/positions";
-import { DndContext, DragOverlay } from "@dnd-kit/core";
-import { useMemo, useRef, useState } from "react";
+import { DndContext } from "@dnd-kit/core";
+import { useMemo, useRef } from "react";
 import { useClickAway } from "react-use";
 import { Grid } from "styled-system/jsx";
 import { parseFen } from "../parsers/parseFen";
 import { Square } from "./Square";
 import { useBoardAria } from "./aria";
-import { Piece } from "./pieces/Piece";
 
 /**
  * @param {{
  *   gameId: string,
  *   positionId: string
  * }} props
- * @returns
  */
 export function Board(props) {
   const { gameId, positionId } = props;
@@ -34,14 +32,11 @@ export function Board(props) {
   });
 
   const { squareRefs, keyboardProps } = useBoardAria();
-  const [draggedPiece, setDraggedPiece] = useState(/** @type {Piece | undefined} */ (undefined));
 
   /**
    * @param {import("@dnd-kit/core").DragEndEvent} event
    */
   function handleDragEnd(event) {
-    setDraggedPiece(undefined);
-
     const { active, over } = event;
 
     if (over) {
@@ -58,17 +53,7 @@ export function Board(props) {
   }
 
   return (
-    <DndContext onDragStart={(e) => setDraggedPiece(e.active.data.current?.piece)} onDragEnd={(e) => handleDragEnd(e)}>
-      <DragOverlay
-        dropAnimation={{
-          duration: 200,
-          easing: "cubic-bezier(0.18, 0.67, 0.6, 1.22)",
-        }}
-        zIndex={1000}
-      >
-        {draggedPiece ? <Piece piece={draggedPiece} /> : null}
-      </DragOverlay>
-
+    <DndContext onDragEnd={(e) => handleDragEnd(e)}>
       <Grid
         role="grid"
         aria-colcount={8}
@@ -86,8 +71,7 @@ export function Board(props) {
           lg: "auto",
         }}
         aspectRatio="square"
-        {...keyboardProps}
-      >
+        {...keyboardProps}>
         {squares.map((square, index) => (
           <Square
             key={square.file + square.rank}
