e5806947171913b5ea754670de4ce8841bd18af0
blunderfest <github@blunderfest.org>
2024-01-26 13:33:13 +0100

    Update origin for prod

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :blunderfest, BlunderfestWeb.Endpoint,
-  url: [host: "blunderfest.org", port: 443],
+  check_origin: ["//blunderfest.org"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Configures Swoosh API Client
