2e97fcdcf8d8009275a3afa3ab0dc806dcf194bf
blunderfest <github@blunderfest.org>
2024-05-21 14:23:43 +0200

    Remove logging

--- lib/blunderfest_web/controllers/page_controller.ex
+++ lib/blunderfest_web/controllers/page_controller.ex
@@ -7,10 +7,8 @@ defmodule BlunderfestWeb.PageController do
   def index(conn, _params) do
     room_code = Nanoid.generate()
 
-    with {:ok, pid} <-
+    with {:ok, _pid} <-
            Horde.DynamicSupervisor.start_child(Blunderfest.Supervisor, {RoomServer, room_code}) do
-      found = Blunderfest.RoomServer.exists?(room_code)
-      Logger.info("Started room #{room_code} with PID #{inspect(pid)}. Exists? #{found}")
       conn |> redirect(to: ~p"/#{room_code}")
     else
       err ->
@@ -21,8 +19,6 @@ defmodule BlunderfestWeb.PageController do
 
   def join(conn, %{"room_code" => room_code}) do
     if Blunderfest.RoomServer.exists?(room_code) do
-      IO.puts("Joining #{room_code}")
-
       conn |> assign(:page_title, room_code) |> render(:index)
     else
       Logger.info("Room #{room_code} not found. Redirecting to index")
